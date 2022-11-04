1b7c85c30a3908c94e5faafa9f8015a41f056ebd
Erak93 <giannicolaciudino@yahoo.it>
2022-11-04 15:10:07 +0100

    task3

--- regex-group-projects/1-group-python-basics-texts-project-user_accounts-Erak93/src/validate_date.py
+++ regex-group-projects/1-group-python-basics-texts-project-user_accounts-Erak93/src/validate_date.py
@@ -1,17 +1,11 @@
 import re
 
 
-def validate_date(date_input):
-    """
-    Date validation rules:
-        - Starts with a number (number should be 1-31)
-        - Then either of these signs (dot '.'  dash '-' slash '/')
-        - Then a number (1-12)
-        - Then again either of these signs (dot '.'  dash '-' slash '/')
-        - then year from 2000 - 2099
-
-    :param date_input: <str> The string containing date to be checked
-    :return: True if date_input respects any of date format. Returns False otherwise
-    """
-    pass
-
+def validate_date():
+    date_input=input("give me a date")
+    sear=re.search('(0[1-9]|[12][0-9]|3[01])([-\/.])(0[1-9]|1[012])([-\/.])(19[0-9][0-9]|20[0-9][0-9])',date_input) 
+    print(bool(sear))
+    
+    
+    
+validate_date()    
