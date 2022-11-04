5710e3e7aafd52301a6caaecd94ea183a094ad5a
Erak93 <giannicolaciudino@yahoo.it>
2022-11-04 13:29:32 +0100

    task 1 updated

--- regex-group-projects/1-group-python-basics-texts-project-user_accounts-Erak93/src/validate_phone_num.py
+++ regex-group-projects/1-group-python-basics-texts-project-user_accounts-Erak93/src/validate_phone_num.py
@@ -4,10 +4,11 @@ def validate_phone_num():
         
    x=str(input("Digit phone number please"))
    search=re.search('\+\d{3}\s\(\d{3}\)\s\d{3}\s\d{4}$',x)
-   if search:
-        print(True)
-   else:
-        print(False)     
+   print(bool(search))
+  # if search:
+     #   print(True)
+  # else:
+     #   print(False)     
 
 
   
