5483eb63d520d1180a61c402614ffc3f20934a20
Mori <morteza.fazel@gmail.com>
2022-11-08 11:22:55 +0100

    ?added to 0

--- regex-group-projects/1-group-python-basics-texts-project-user_accounts-Erak93/src/validate_date.py
+++ regex-group-projects/1-group-python-basics-texts-project-user_accounts-Erak93/src/validate_date.py
@@ -3,7 +3,7 @@ import re
 
 def validate_date():
     date_input=input("give me a date")
-    sear=re.search('(0[1-9]|[12][0-9]|3[01])([-\/.])(0[1-9]|1[012])([-\/.])(19[0-9][0-9]|20[0-9][0-9])',date_input) 
+    sear=re.search('(0?[1-9]|[12][0-9]|3[01])([-\/.])(0?[1-9]|1[012])([-\/.])(19[0-9][0-9]|20[0-9][0-9])',date_input) 
     print(bool(sear))
     
     
