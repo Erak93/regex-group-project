98b5009d373721dd01e1093d3c56c8793291703c
Erak93 <giannicolaciudino@yahoo.it>
2022-11-11 12:07:59 +0100

    commit

--- regex-group-projects/1-group-python-basics-texts-project-user_accounts-Erak93/src/validate_password.py
+++ regex-group-projects/1-group-python-basics-texts-project-user_accounts-Erak93/src/validate_password.py
@@ -3,14 +3,22 @@ import re
 
 # Complete this function to check if passwords respect the format
 def validate_password(pwd_input):
-    test=re.search('^.*(?=.*[a-z])(?=.*[A_Z])(?=\w{8,30})(?=.*\d)(?=.*[!\*\-_\.]).*$',pwd_input)
+    test=re.search('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[-_\.!*])[A-Za-z\d\-\.!\*_]{8,30}$',pwd_input)
     if test:
         return True
 
     return False
 
 
-    """
+print(validate_password("Madisson45!"))
+
+
+
+
+
+
+
+"""
     At least 8 characters
     At most 30 characters
     At least one Uppercase
@@ -22,7 +30,7 @@ def validate_password(pwd_input):
     :param pwd_input:
     :return: True if the pwd_input respect the pattern, False otherwise
     """
-    pass
+pass
 
 
 
